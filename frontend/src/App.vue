2f004456dd1a3bde73163b977bf7a4a05d127dc0
seiya303 <seiya.u77@gmail.com>
2021-10-31 05:53:05 +0900

    authenticated api response but can't show the list of visitos yet

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="nav">
-    <NavbarComponent :requestUser="requestUser"/>
+    <NavbarComponent />
   </div>
-  <router-view :requestUser="requestUser"/>
+  <router-view />
 </template>
 
 <script>
@@ -13,28 +13,37 @@ export default {
   components: {
     NavbarComponent,
   },
-  data() {
-    return {
-      requestUser: null, // propsを使わない方が安全か？
-    };
-  },
   methods: {
+    async getToken() {
+      let endpont = "/auth/token/login/";
+      try {
+        const data = {username: process.env.VUE_APP_USERNAME, password: process.env.VUE_APP_PASSWORD};
+        const response = await axios.post(endpont, data);
+        const requestAuthtoken = response.data["auth_token"];
+        window.localStorage.setItem("auth_token", requestAuthtoken);
+        console.log('auth_token', window.localStorage.getItem("auth_token"));
+      } catch (error) {
+        console.log(error.response);
+        alert(error.response.statusText);
+      }
+    },
     async setUserInfo() {
       // add the username of the current user to localStorage
       try {
         const response = await axios.get("/auth/users/me/");
         const requestUser = response.data["username"];
         window.localStorage.setItem("username", requestUser);
-        this.requestUser = window.localStorage.getItem("username");
-        console.log(this.requestUser)
+        // this.requestUser = window.localStorage.getItem("username");
+        console.log('requestUser:', window.localStorage.getItem("username"));
       } catch (error) {
         console.log(error.response);
         console.log(this.requestUser)
-        // alert(error.response.statusText);
+        alert(error.response.statusText);
       }
     },
   },
   created() {
+    this.getToken();
     this.setUserInfo();
   },
 };
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,28 +1,59 @@
 <template>
     <div>
         <h1>Logged in as: {{ String(requestUser) }}</h1>
+        <b-table striped hover :items="visitors"></b-table>
     </div>
 </template>
 
 <script>
-// import { axios } from "@/common/api.service.js";
+import { axios } from "@/common/api.service.js";
 export default {
     name: "Manager",
-    props: {
-        requestUser: {
-            type: String,
-        },
+    data() {
+        return {
+            requestUser: window.localStorage.getItem("username"),
+            visitors: [],
+            next: null,
+            loadingVisitors: false,
+        };
     },
     methods: {
+        async getVisitors() {
+            // make a GET Request to the visitors list endpoint and populate the visitors array
+            const headers = {"Authorization": `Token ${window.localStorage.getItem("auth_token")}`}
+            let endpoint = "/api/v1/lookup/";
+            if (this.next) {
+                endpoint = this.next;
+            }
+            this.loadingVisitors = true;
+            try {
+                const response = await axios.get(endpoint, {headers});
+                this.visitors.push(...response.data);
+                console.log('visitors', this.visitors);
+                this.loadingVisitors = false;
+                if (response.data.next) {
+                    this.next = response.data.next
+                } else {
+                    this.next = null;
+                }
+            } catch (error) {
+                console.log(error.response);
+                alert(error.response.statusText);
+            }
+        },
         checkAuth () {
             if (this.requestUser === null) {
                 alert("invalid access");
                 this.$router.push({ name: "page-not-found" });
             }
         },
+
     },
     created() {
+        console.log('requestUser(Home):', window.localStorage.getItem("username"));
+        document.title = "Manager"
         this.checkAuth();
+        this.getVisitors();
     },
 }
 </script>
